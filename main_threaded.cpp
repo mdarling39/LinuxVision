e3bd9b350a439115d8e80dde04fbffcbd096d6e3
mdarling39 <mdarling@calpoly.edu>
2013-10-28 21:13:05 -0700

    Moved JPEG decompression to the processing thread. Appear to be achieving 30 fps in both capture and processing.

--- main_threaded.cpp
+++ main_threaded.cpp
@@ -24,23 +24,25 @@ const int IM_WIDTH = 640;
 const int IM_HEIGHT = 480;
 const unsigned char THRESHOLD = 225;
 const unsigned char THRESHOLD_MAXVAL = 255;
-#define LIVE_CAPTURE 1
 
-#if LIVE_CAPTURE
+
+struct v4l2Parms parms;
+#define BUF_SZ 4
+struct v4l2_buffer buf;
+void* buff_ptr;
+struct user_buffer_t{
+    void* ptr[BUF_SZ];
+    int buf_idx;
+    int buf_last;
+} user_buffer;
+
+
 Mat frame(Size(IM_WIDTH,IM_HEIGHT),CV_8UC3);
-#else
-Mat frame = imread("/home/mdarling/Desktop/CompleteVision_MAIN/SampleImage.jpeg",CV_LOAD_IMAGE_COLOR);
-#endif
 Mat gray(Size(IM_WIDTH,IM_HEIGHT),CV_8UC1);
 Mat binary(Size(IM_WIDTH,IM_HEIGHT),CV_8UC1);
 
-
 vector<vector<Point> > contours;
-vector<Vec4i> hierarchy;
 
-ProfilerTool timerA, timerB;
-time_t time_tic,time_toc;
-double time_tot = 0;
 FPSCounter fps(15);
 
 void custom_v4l2_init(void*);
@@ -55,10 +57,6 @@ pthread_cond_t done_using_frame = PTHREAD_COND_INITIALIZER;
 pthread_cond_t done_capturing = PTHREAD_COND_INITIALIZER;
 pthread_cond_t done_processing = PTHREAD_COND_INITIALIZER;
 
-int BUFF_FULL = 0;
-int DONE_PROCESSING = 1;
-int DONE_CAPTURING = 0;
-
 int main()
 {
     // Initialize pthreads
@@ -70,10 +68,7 @@ int main()
 
     pthread_join(capture_thread,NULL); // Let the capture thread end the program
 
-    // report timings
-    printf ("\nCapture time: %f milliseconds,time.\n",(float)time_tot * 1000.0);
-    printf ("TimerA took: %d clicks (%f milliseconds).\n",(int)timerA.n_clocks(),timerA.ms());
-    printf ("TimerB took: %d clicks (%f milliseconds).\n",(int)timerB.n_clocks(),timerB.ms());
+    cout << "DONE" << endl;
 
     return 0;
 }
@@ -81,7 +76,7 @@ int main()
 void *capture(void*)
 {
     // Configure camera properties
-    struct v4l2Parms parms;
+
     v4l2_set_defalut_parms(&parms);
     char devName[15];
     sprintf(devName,"/dev/video%d",DEVICE);
@@ -97,52 +92,31 @@ void *capture(void*)
     v4l2_init_device(&parms);
     v4l2_start_capturing(&parms);
 
+    // Cycle through and fill up the image pointers
+    for (int i=0; i<2*BUF_SZ; i++)
+    {
+        while(0 != v4l2_wait_for_data(&parms)){}
+        v4l2_fill_buffer(&parms, &buf, &user_buffer.ptr[buf.index]);
+        v4l2_queue_buffer(&parms, &buf);
+    }
 
     //  Capture frames
-    for (int iters=0; iters<500; iters++)
+    while(1)
     {
-
-        time(&time_tic);
         if ( 0 != v4l2_wait_for_data(&parms))   // calls select()
             continue;                           // retry on EAGAIN
 
-        struct v4l2_buffer buf;
-        void* buff_ptr;
-
-        v4l2_fill_buffer(&parms, &buf, &buff_ptr); // dequeue buffer
-
-        ///Show the rate at which we are taking images from the camera
-        //double fps_cnt=fps.fps();
-        //cout << "\r" "FPS: " << fps_cnt << flush;
-
-        // May not want to wait for processing function to finish.
-        if ( DONE_PROCESSING < 1)
-        {
-            v4l2_queue_buffer(&parms, &buf);
-            --iters; // don't count this iteration
-            continue;
-        }
-
-        // Wait until processing thread is done accessing the buffer
         pthread_mutex_lock(&framelock_mutex);
-        if (BUFF_FULL > 0)
-            pthread_cond_wait(&done_using_frame,&framelock_mutex);
-
-        // Decode the JPEG image and store to the framebuffer
-        v4l2_process_image(frame, buff_ptr);
-        time(&time_toc);
-        time_tot += difftime(time_toc, time_tic);
-
-        BUFF_FULL = 1;
+        user_buffer.buf_idx = buf.index;
+        user_buffer.buf_last = (buf.index - 1) % BUF_SZ;
+        v4l2_fill_buffer(&parms, &buf, &user_buffer.ptr[user_buffer.buf_idx]); // dequeue buffer
+        v4l2_queue_buffer(&parms, &buf);
         pthread_cond_broadcast(&done_saving_frame);
         pthread_mutex_unlock(&framelock_mutex);
 
-        v4l2_queue_buffer(&parms, &buf); // queue the buffer for next capture
+//        double fps_cnt=fps.fps();
+//        cout << "\r" "FPS: " << fps_cnt << flush;
 
-        pthread_mutex_lock(&taketurns_mutex);
-        DONE_CAPTURING = 1; DONE_PROCESSING = 0;
-        pthread_cond_broadcast(&done_capturing);
-        pthread_mutex_unlock(&taketurns_mutex);
     }
 
     v4l2_stop_capturing(&parms);
@@ -154,49 +128,32 @@ void *capture(void*)
 
 void *processing(void*)
 {
+sleep(1); //Ensure that the capture thread starts us off
 while(1)
 {
-    // Wait until there is a new image worth processing
-    pthread_mutex_lock(&taketurns_mutex);
-    if (DONE_CAPTURING < 1)
-        pthread_cond_wait(&done_capturing,&taketurns_mutex);
-    pthread_mutex_unlock(&taketurns_mutex);
 
-
+    /// TODO:  Include some kind of protection to make sure we dont' repeatedly process the same frame
     pthread_mutex_lock(&framelock_mutex);
-    if (BUFF_FULL < 1)
-        pthread_cond_wait(&done_saving_frame,&framelock_mutex);
+    // Decode JPEG image stored in the most recently dequeued buffer
+    v4l2_process_image(frame, user_buffer.ptr[user_buffer.buf_last]);
+    pthread_mutex_unlock(&framelock_mutex);
 
-    timerA.tic();
     const int mixCh[]= {2,0};
     mixChannels(&frame,1,&gray,1,mixCh,1);  // For now, OpenCV's implementation is faster
     //gray = gray.clone(); // force a hard copy (This might be a little bit expensive)
                          // consider implementing a circular image buffer to skip this step
 
 
-
-    BUFF_FULL = 0;
-    pthread_cond_broadcast(&done_using_frame);
-    pthread_mutex_unlock(&framelock_mutex);
-
-    timerB.tic();
     threshold(gray,binary,215,255,THRESH_BINARY);
-    const cv::Mat kernel(3,3,CV_8UC1,Scalar(255));
+    const cv::Mat kernel(5,5,CV_8UC1,Scalar(255));
     findContours(binary,contours,CV_RETR_EXTERNAL,CV_CHAIN_APPROX_SIMPLE);
-    timerB.toc();
-    timerA.toc();
 
     double fps_cnt=fps.fps();
-    cout << "\r" "FPS: " << fps_cnt << flush;
+    printf("\r  FPS:  %2.2f   contours: %4d",fps_cnt,contours.size());
+    fflush(stdout);
 
     //imshow("drawing",gray);
     //waitKey(1);
-
-
-    pthread_mutex_lock(&taketurns_mutex);
-    DONE_PROCESSING = 1; DONE_CAPTURING = 0;
-    pthread_cond_broadcast(&done_processing);
-    pthread_mutex_unlock(&taketurns_mutex);
 }
 pthread_exit(NULL);
 }
